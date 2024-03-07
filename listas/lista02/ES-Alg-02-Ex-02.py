f8d5796259cb9197143d46188770c298e2a55d5a
Eliézer da Silva <eliezerdasilva2005@gmail.com>
2024-03-07 15:16:22 -0300

    Update activity

--- listas/lista02/ES-Alg-02-Ex-02.py
+++ listas/lista02/ES-Alg-02-Ex-02.py
@@ -7,8 +7,14 @@ respectivamente. Os valores de horas, minutos e segundos devem ser formatados to
 dois dígitos, sendo obrigatória a inclusão do dígito 0 para valores menores que 10
 '''
 segundoDados = int(input("informe o tempo em segundos"))
-dias = segundoDados//86400
-horas = (segundoDados%86400)//3600
-minutos = ((86400%segundoDados) % 3600)//60
-segundos = ((86400%segundoDados) % 3600)%60
+
+dias = segundoDados // 86400
+segundoDados %= 86400
+
+horas = segundoDados // 3600
+segundoDados %= 3600
+
+minutos = segundoDados // 60
+segundos = segundoDados % 60
+
 print("Tempo equivalente: {:02d}:{:02d}:{:02d}:{:02d}".format(dias, horas, minutos, segundos))
